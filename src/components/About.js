eb54e5575024faec9e57a0a5a771cf5d8f837d71
HafsaI <hafsairfan80@gmail.com>
2023-01-15 22:14:21 +0500

    updates

--- src/components/About.js
+++ src/components/About.js
@@ -8,7 +8,7 @@ export default function About() {
         <div className = "about">
         <div className = "about--text">
          <h6 className = "about--title">about me  </h6> 
-         <p className = "about--para"> Hello! I am Hafsa Irfan. I am in my final year of BS Computer Science with a minor in Communication and Design. I have learned various technologies, programming languages, and problem-solving skills since last few years. I am interested in the fields of Software Engineering, Machine Learning and Product Design. </p>
+         <p className = "about--para"> Hello! I am Hafsa Irfan. I am in my final year of BS Computer Science with a minor in Communication and Design. I have learned various technologies, programming languages, and problem-solving skills since last few years. I am interested in the fields of Software Engineering, Machine Learning and Product Management. </p>
 
         <p className = "about--para">I am a detail-oriented, hardworking and organized person. I hope to work with companies on impactful projects. </p>
 
